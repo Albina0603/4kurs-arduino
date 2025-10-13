cf1d55a045d4645406b5bc4c2898334c0e161a4d
albina0603 <asanovaalbina3@github.com>
2025-10-13 16:25:12 +0600

    lab2

---
+++ lab2/lab2.ino
@@ -0,0 +1,18 @@
+const int ledPin = 7;
+const int buttonPin = 12;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == LOW) {
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    delay(500);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}

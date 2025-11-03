35e6d0f1c1ed6620bc6fbec13fdc3d678e8fecb9
albina0603 <asanovaalbina3@github.com>
2025-11-03 16:28:51 +0600

    with leraaaa

---
+++ muz/muz.ino
@@ -0,0 +1,34 @@
+const int ledPin = 9;
+const int buzzerPin = 8;
+const int shockPin = A3;
+
+
+int melody[] = {262, 294, 330, 349, 392, 440, 494, 523};
+int noteDur = 200; 
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(shockPin, INPUT);
+}
+
+void playSong() {
+  for (int i = 0; i < 8; i++) {
+    tone(buzzerPin, melody[i]);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+
+    noTone(buzzerPin);
+    digitalWrite(ledPin, LOW);
+    delay(50);
+  }
+}
+
+void loop() {
+  int shockValue = digitalRead(shockPin);
+
+  if (shockValue == HIGH) {
+    playSong();
+  }
+}
+  
